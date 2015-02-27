5b0a89a52085a3d7e8c392ad80cdc7b9723fb15c
John Paul Welsh <wearing2raincoats@yahoo.com>
2015-02-26 22:41:07 -0500

    FizzBuzz done in one line, bitch

---
+++ fizzbuzz/FizzBuzz.scala
@@ -0,0 +1,1 @@
+println((1 to 100) map (_.toString) map ((s: String) => if (s.toInt % 3 == 0 && s.toInt % 5 == 0) "FizzBuzz" else if (s.toInt % 3 == 0) "Fizz" else if (s.toInt % 5 == 0) "Buzz" else s))
