28219ad36019c7241b24e464648471c03f7b8ce9
John Paul Welsh <wearing2raincoats@yahoo.com>
2015-02-28 15:35:45 -0500

    Added commented-out readable version

--- fizzbuzz/FizzBuzz.scala
+++ fizzbuzz/FizzBuzz.scala
@@ -1,1 +1,16 @@
+// One-line version
 println((1 to 100) map (_.toString) map ((s: String) => if (s.toInt % 3 == 0 && s.toInt % 5 == 0) "FizzBuzz" else if (s.toInt % 3 == 0) "Fizz" else if (s.toInt % 5 == 0) "Buzz" else s))
+
+// Readable version
+/*
+println(
+  (1 to 100) map
+  (_.toString) map (
+    (s: String) =>
+    if (s.toInt % 3 == 0 && s.toInt % 5 == 0) "FizzBuzz"
+    else if (s.toInt % 3 == 0) "Fizz"
+    else if (s.toInt % 5 == 0) "Buzz"
+    else s
+  )
+)
+*/
